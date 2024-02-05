ebb63ed8b9fd5e8b7221c6fbb3f6742a928eca47
Мифтахов Олег <olegmif@gmail.com>
2024-02-05 21:28:26 +0500

    Exp menu

---
+++ src/components/exp-menu/exp-menu.tsx
@@ -0,0 +1,5 @@
+import {FC} from "react";
+
+const ExpMenu: FC<ExpMenuProps> = ({item}) => {
+    return <div>ExpMenu</div>
+}
---
+++ src/components/exp-menu/exp-menu.types.ts
@@ -0,0 +1,5 @@
+import {MenuItem} from "@components/menu-bar/menu-bar.types";
+
+export interface ExpMenu {
+    item: MenuItem
+}
