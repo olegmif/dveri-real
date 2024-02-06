08809dfdf55959448430899966c6f79e4cc45eed
Мифтахов Олег <olegmif@gmail.com>
2024-02-06 20:53:12 +0500

    menu styles

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -3,16 +3,23 @@
 import {FC, useState} from "react";
 import cn from "classnames";
 import Link from "next/link";
+import { Montserrat } from "next/font/google";
 import { MenuProps } from "./menu.types";
 import styles from "./menu.module.scss";
+
+const lato = Montserrat({
+  weight: "600",
+  subsets: ['cyrillic']
+})
 
 const Menu: FC<MenuProps> = ({ item, level = 0 }) => {
   const [expanded, setExpanded] = useState<boolean>(false);
   const title = () => {
+    const classes = cn(styles.title, lato.className);
     if (!item.title) return null;
-    if (item.items) return <button onClick={() => setExpanded((prev => !prev))}>{item.title}</button>;
-    if (item.path) return <Link href={item.path}>{item.title}</Link>;
-    return <span>{item.title}</span>;
+    if (item.items) return <button className={classes} onClick={() => setExpanded((prev => !prev))}>{item.title}</button>;
+    if (item.path) return <Link className={classes} href={item.path}>{item.title}</Link>;
+    return <span className={classes}>{item.title}</span>;
   };
 
   return (
